682b0b918fe8684a297ec3138d0bf9aa12b23938
Fabian Johansson <fabianjohansson@pop-os.localdomain>
2020-02-27 13:31:53 +0100

    added boostrap carousel cards

--- app/public/wp-content/themes/understrap-child/front-page.php
+++ app/public/wp-content/themes/understrap-child/front-page.php
@@ -26,72 +26,44 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="wrapper" id="index-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-        <h1>EEP EEP</h1>
-
 		<div class="row">
-
-			<!-- Do the left sidebar check and opens the primary div -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-			<?php get_template_part( 'global-templates/jbf-sidebar-check' ); ?>
-
-			<main class="site-main" id="main">
-
-                <?php 
-                
-                $args = array(
-                    'post_type'      => 'object',
-                    'posts_per_page' => '12'
-                );
-                $loop = new WP_Query( $args );
-
-                while( $loop->have_posts() ) :
-                    $loop->the_post();
-
-                    echo get_the_title()."<br>";
-                    echo get_the_ID();
-
-                    $children = get_children([
-                        'post_parent' => $id,
-                        'post_status' => 'inherit'
-                    ]);
-
-                    $images = [];
-
-                    foreach ($children as $img) $images[] = $img;
-
-                ?>
+        
+            <div class="col-md-8">
                 <br>
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="width: 800px"> 
-                    <div class="carousel-inner">
+                <main class="site-main" id="main">
 
-                    <?php foreach($images as $i=>$img) : ?>
-                        <div class="carousel-item <?php echo $i == 0 ? "active" : ""?>" style="height: 400px">
-                            <img class="d-block" style="width: 800px" src="<?php echo $img->guid ?>">
-                        </div>
-                    <?php endforeach; ?>
+                    <?php 
+                    
+                    //Get all objects
+                    $args = array(
+                        'post_type'      => 'object',
+                        'posts_per_page' => '12'
+                    );
 
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-                <br>
-                <?php endwhile; ?>
+                    $loop = new WP_Query( $args );
 
+                    //Main loop
+                    while( $loop->have_posts() ) :
+                        $loop->the_post();
 
-			</main><!-- #main -->
+                        get_template_part('object-templates/object-card');
+                    ?>
 
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
+                    <?php 
+                        //End main loop 
+                        endwhile;
+                    ?>
 
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+                </main><!-- #main -->
 
+                <!-- The pagination component -->
+                <?php understrap_pagination(); ?>
+
+            </div><!-- .col -->
+
+            <!-- Do the left sidebar check and opens the primary div -->
+            <?php get_template_part( 'global-templates/jbf-sidebar-check' ); ?>
+            
 		</div><!-- .row -->
 
 	</div><!-- #content -->
