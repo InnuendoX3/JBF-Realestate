3a43c8e4ba4b3a333b8b171a8493712c3b9a9875
Johan Garzon <johanprograma@hotmail.com>
2020-03-04 16:05:35 +0100

    Plugin seeme to remove Author capability to remove posts

--- app/public/wp-content/plugins/jbf-capabilities/jbf-capabilities.php
+++ app/public/wp-content/plugins/jbf-capabilities/jbf-capabilities.php
@@ -1,7 +1,24 @@
 <?php
 /**
- * Plugin Name: JBF Capabilities
+ * Plugin Name: JBF Capabilities (Remove)
  * Description: Disable Author for publishing objects. Disable Editor for deleting objects.
  * Version: 1.0
  * Author: Johan - Bryan - Fabian
  */
+
+add_action( 'init', 'wpsites_remove_author_capabilities' );
+
+function wpsites_remove_author_capabilities() {
+
+    $author = get_role( 'author' );
+
+    $caps = array(
+        'delete_posts',
+        'delete_published_posts',
+    );
+
+    foreach ( $caps as $cap ) {
+
+        $author->remove_cap( $cap );
+    }
+}
