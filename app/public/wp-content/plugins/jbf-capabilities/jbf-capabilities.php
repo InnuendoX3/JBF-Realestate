917fc9c9024dcca13084b8a6af219c4f2eb8f5db
Johan Garzon <johanprograma@hotmail.com>
2020-03-04 15:49:57 +0100

    Create an empty plugin: jbg-capabilities

---
+++ app/public/wp-content/plugins/jbf-capabilities/jbf-capabilities.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: JBF Capabilities
+ * Description: Disable Author for publishing objects. Disable Editor for deleting objects.
+ * Version: 1.0
+ * Author: Johan - Bryan - Fabian
+ */
