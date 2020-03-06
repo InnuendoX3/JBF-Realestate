4c2fa79526d5b7f3b26841e960ce17a38e1a1e71
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-03-06 15:06:33 +0100

    added picture for our hero page

---
+++ app/public/wp-content/themes/understrap-child-master/global-templates/hero.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Hero setup.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<div class="wrapper" id="wrapper-hero">
+    <div class="jumbotron jumbotron-fluid">
+        
+    </div>
+
+</div>
